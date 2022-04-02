cbd008bfcfecdf5644c30c84641da8ffcbf8198f
youtics <youtics@gmail.com>
2022-04-02 08:16:52 -0300

    version de prueba flutter con wordpress

---
+++ lib/common/constants.dart
@@ -0,0 +1,28 @@
+library constants;
+
+// Your wordpress website URL
+const String WORDPRESS_URL = "http://labancaria2.obliviondev.com.ar/";
+
+// Featured category ID (for Home Screen top section)
+const int FEATURED_ID = 2;
+
+// Tab 2 page category name
+const String PAGE2_CATEGORY_NAME = "Food";
+
+// Tab 2 page category ID
+const int PAGE2_CATEGORY_ID = 3;
+
+// Custom categories in search tab
+// Array in format
+// ["Category Name", "Image Link", "Category ID"]
+const List<dynamic> CUSTOM_CATEGORIES = [
+  ["Food", "assets/boxed/lifestyle.png", 3],
+  ["Fruit", "assets/boxed/fashion.png", 4],
+  ["Pasta", "assets/boxed/music.png", 6],
+  ["Pizza", "assets/boxed/photography.png", 7],
+  ["Salads", "assets/boxed/sport.png", 8],
+  ["Dessert", "assets/boxed/world.png", 2],
+  ["Meat & Fish", "assets/boxed/health.png", 5],
+  ["Video", "assets/boxed/travel.png", 10],
+  ["Video Recipies", "assets/boxed/recipies.png", 11],
+];
